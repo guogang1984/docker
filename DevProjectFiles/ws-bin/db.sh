37caabebb881fbbb02adbb1e95320836302bd562
guogang <guogang@topflames.com>
2017-10-30 18:05:56 +0800

    db脚本分离

--- DevProjectFiles/ws-bin/db.sh
+++ DevProjectFiles/ws-bin/db.sh
@@ -6,29 +6,39 @@
 DOCKER_DB_NAME=${DOCKER_PROJ_WEB_NAME}
 
 # -----------------------------------------------------------------------------
-DOCKER_FILE_DB=~/DevProjectFiles/ws-docker/docker-compose-db-redis3.yml
+DOCKER_FILE_DB_REDIS3=~/DevProjectFiles/ws-docker/docker-compose-db-redis3.yml
+DOCKER_FILE_DB_MYSQL=~/DevProjectFiles/ws-docker/docker-compose-db-mysql.yml
+DOCKER_FILE_DB_MONGO=~/DevProjectFiles/ws-docker/docker-compose-db-mongo.yml
 # -----------------------------------------------------------------------------
 
 CUR_ALL_TIME=`date "+%Y%m%d%H%M%S"`
 
 
-DOCKER_DB_CID=`docker ps -f name=db-mysql -q`
-DOCKER_DB_BACK_FILE=/DevProjectFiles/ws-back/${DOCKER_DB_NAME}_${CUR_ALL_TIME}.sql
-DOCKER_DB_BACK_OPTS="--user=root --password=root --databases ${DOCKER_DB_NAME}"
-DOCKER_DB_BACK_OPTS="-h localhost -P 3306 --complete-insert --extended-insert=false --add-drop-table --skip-opt --result-file=${DOCKER_DB_BACK_FILE} ${DOCKER_DB_BACK_OPTS}"
-DOCKER_DB_CMD=`echo  -e "mysqldump ${DOCKER_DB_BACK_OPTS}"`
-
 if [ "$1" = "back" ] ; then
   shift
-  echo -e "export db '${DOCKER_DB_NAME}' cmd: " && echo -e "mysqldump ${DOCKER_DB_BACK_OPTS}"
-  docker exec -it ${DOCKER_DB_CID} ${DOCKER_DB_CMD} > /dev/null
-  ls -lh ~/DevProjectFiles/ws-back/${DOCKER_DB_NAME}_${CUR_ALL_TIME}.sql
+  if [ "$1" = "mysql" ] ; then
+    DOCKER_DB_CID=`docker ps -f name=db-mysql -q`
+    DOCKER_DB_BACK_FILE=/DevProjectFiles/ws-back/${DOCKER_DB_NAME}_${CUR_ALL_TIME}.sql
+    DOCKER_DB_BACK_OPTS="--user=root --password=root --databases ${DOCKER_DB_NAME}"
+    DOCKER_DB_BACK_OPTS="-h localhost -P 3306 --complete-insert --extended-insert=false --add-drop-table --skip-opt --result-file=${DOCKER_DB_BACK_FILE} ${DOCKER_DB_BACK_OPTS}"
+    DOCKER_DB_CMD=`echo  -e "mysqldump ${DOCKER_DB_BACK_OPTS}"`
+    echo -e "export db '${DOCKER_DB_NAME}' cmd: " && echo -e "mysqldump ${DOCKER_DB_BACK_OPTS}"
+    docker exec -it ${DOCKER_DB_CID} ${DOCKER_DB_CMD} > /dev/null
+    ls -lh ~/DevProjectFiles/ws-back/${DOCKER_DB_NAME}_${CUR_ALL_TIME}.sql
+  fi
 elif [ "$1" = "start" ] ; then
   shift
-  [ "$1" = "mysql" ] && `docker-compose -f $DOCKER_FILE_DB up -d ` && exit 1
+
+  docker-compose -f $DOCKER_FILE_DB_REDIS3 up -d 
+
+  [ "$1" = "mysql" ] && `docker-compose -f $DOCKER_FILE_DB_MYSQL up -d ` && exit 1
+  [ "$1" = "mongo" ] && `docker-compose -f $DOCKER_FILE_DB_MONGO up -d ` && exit 1
 elif [ "$1" = "stop" ] ; then   
   shift
-  [ "$1" = "mysql" ] && `docker-compose -f $DOCKER_FILE_DB down ` && exit 1
+  docker-compose -f $DOCKER_FILE_DB_REDIS3 down
+
+  [ "$1" = "mysql" ] && `docker-compose -f $DOCKER_FILE_DB_MYSQL down ` && exit 1
+  [ "$1" = "mongo" ] && `docker-compose -f $DOCKER_FILE_DB_MONGO down ` && exit 1
 else
   echo "Usage: db-mysql.sh ( commands ... )   ${CUR_ALL_TIME}"
   echo "env: database=${DOCKER_DB_NAME}"   
